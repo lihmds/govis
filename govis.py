4f0c29d5c47d49251299e724d7e3f22b74f96c5f
lihmds <lithiumhmds@protonmail.com>
2020-07-15 10:32:59 +0200

    remove most of unused code in govis.py

--- govis.py
+++ govis.py
@@ -21,8 +21,7 @@ model_variables_prefix = "nets/g170-b6c96-s175395328-d26788732/saved_model/varia
 model_config_json = "nets/g170-b6c96-s175395328-d26788732/model.config.json"
 name_scope = "swa_model"
 
-# Hardcoded max board size
-pos_len = 19
+pos_len = 19 # Hardcoded max board size
 
 with open(model_config_json) as f:
   model_config = json.load(f)
@@ -30,21 +29,7 @@ with open(model_config_json) as f:
 with tf.compat.v1.variable_scope(name_scope):
   model = Model(model_config,pos_len,{})
 
-policy0_output = tf.nn.softmax(model.policy_output[:,:,0])
-policy1_output = tf.nn.softmax(model.policy_output[:,:,1])
 value_output = tf.nn.softmax(model.value_output)
-scoremean_output = 20.0 * model.miscvalues_output[:,0]
-scorestdev_output = 20.0 * tf.math.softplus(model.miscvalues_output[:,1])
-lead_output = 20.0 * model.miscvalues_output[:,2]
-vtime_output = 150.0 * tf.math.softplus(model.miscvalues_output[:,3])
-ownership_output = tf.tanh(model.ownership_output)
-scoring_output = model.scoring_output
-futurepos_output = tf.tanh(model.futurepos_output)
-seki_output = tf.nn.softmax(model.seki_output[:,:,:,0:3])
-seki_output = seki_output[:,:,:,1] - seki_output[:,:,:,2]
-seki_output2 = tf.sigmoid(model.seki_output[:,:,:,3])
-scorebelief_output = tf.nn.softmax(model.scorebelief_output)
-sbscale_output = model.sbscale3_layer
 
 class GameState:
   def __init__(self,board_size):
@@ -52,66 +37,6 @@ class GameState:
     self.board = Board(size=board_size)
     self.moves = []
     self.boards = [self.board.copy()]
-
-def fetch_output(session, gs, rules, fetches):
-  bin_input_data = np.zeros(shape=[1]+model.bin_input_shape, dtype=np.float32)
-  global_input_data = np.zeros(shape=[1]+model.global_input_shape, dtype=np.float32)
-  pla = gs.board.pla
-  opp = Board.get_opp(pla)
-  move_idx = len(gs.moves)
-  model.fill_row_features(gs.board,pla,opp,gs.boards,gs.moves,move_idx,rules,bin_input_data,global_input_data,idx=0)
-  outputs = session.run(fetches, feed_dict={
-    model.bin_inputs: bin_input_data,
-    model.global_inputs: global_input_data,
-    model.symmetries: [False,False,False],
-    model.include_history: [[1.0,1.0,1.0,1.0,1.0]]
-  })
-  return [output[0] for output in outputs]
-
-def get_layer_values(session, gs, rules, layer, channel):
-  board = gs.board
-  [layer] = fetch_output(session,gs,rules=rules,fetches=[layer])
-  layer = layer.reshape([model.pos_len * model.pos_len,-1])
-  locs_and_values = []
-  for y in range(board.size):
-    for x in range(board.size):
-      loc = board.loc(x,y)
-      pos = model.loc_to_tensor_pos(loc,board)
-      locs_and_values.append((loc,layer[pos,channel]))
-  return locs_and_values
-
-def get_input_feature(gs, rules, feature_idx):
-  board = gs.board
-  bin_input_data = np.zeros(shape=[1]+model.bin_input_shape, dtype=np.float32)
-  global_input_data = np.zeros(shape=[1]+model.global_input_shape, dtype=np.float32)
-  pla = board.pla
-  opp = Board.get_opp(pla)
-  move_idx = len(gs.moves)
-  model.fill_row_features(board,pla,opp,gs.boards,gs.moves,move_idx,rules,bin_input_data,global_input_data,idx=0)
-
-  locs_and_values = []
-  for y in range(board.size):
-    for x in range(board.size):
-      loc = board.loc(x,y)
-      pos = model.loc_to_tensor_pos(loc,board)
-      locs_and_values.append((loc,bin_input_data[0,pos,feature_idx]))
-  return locs_and_values
-
-def get_pass_alive(board, rules):
-  pla = board.pla
-  opp = Board.get_opp(pla)
-  area = [-1 for i in range(board.arrsize)]
-  nonPassAliveStones = False
-  safeBigTerritories = True
-  unsafeBigTerritories = False
-  board.calculateArea(area,nonPassAliveStones,safeBigTerritories,unsafeBigTerritories,rules["multiStoneSuicideLegal"])
-
-  locs_and_values = []
-  for y in range(board.size):
-    for x in range(board.size):
-      loc = board.loc(x,y)
-      locs_and_values.append((loc,area[loc]))
-  return locs_and_values
 
 def main(session):
   board_size = 19
