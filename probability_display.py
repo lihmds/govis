83a12291a3a78ad87e293099b1d971665928c74d
lihmds <lithiumhmds@protonmail.com>
2020-07-28 17:03:48 +0200

    convert BoardPainter into a one-time BoardForeground object

--- probability_display.py
+++ probability_display.py
@@ -3,39 +3,28 @@ import numpy as np
 import pyglet
 from board import Board
 from stochastic_board import StochasticBoard
-from board_graphics import BoardBackground, BoardPainter
+from board_graphics import BoardBackground, BoardForeground
 
 class ProbabilityDisplay:
   def __init__(self, window_size, board_size):
     self.window_size = window_size
     self.board_size = board_size
     self.window = pyglet.window.Window(window_size, window_size)
-    self.painter = BoardPainter(window_size, board_size)
     highest_probability_range = [1/len(StochasticBoard.colors), 1]
-    self.highest_probability_to_opacity = interp1d(highest_probability_range, BoardPainter.opacity_range)
+    self.highest_probability_to_opacity = interp1d(highest_probability_range, BoardForeground.opacity_range)
 
   def update(self, probabilities):
     self.window.dispatch_events()
     background = BoardBackground(self.window_size)
     background.draw()
-    self.draw_tiles(probabilities)
+    self.draw_foreground(probabilities)
     self.window.flip()
 
-  def draw_tiles(self, probabilities):
-    self.painter.begin_batch()
+  def draw_foreground(self, probabilities):
+    foreground = BoardForeground(self.window_size, self.board_size)
     for x in range(self.board_size):
       for y in range(self.board_size):
-        self.draw_tile(x, y, probabilities[x, y])
-    self.painter.end_batch()
-
-  def draw_tile(self, x, y, probabilities):
-    opacity = self.highest_probability_to_opacity(np.max(probabilities))
-    dominant_color = np.argmax(probabilities)
-    if dominant_color == Board.EMPTY:
-      self.painter.draw_intersection(x, y, opacity)
-    elif dominant_color == Board.BLACK:
-      self.painter.draw_black_stone(x, y, opacity)
-    elif dominant_color == Board.WHITE:
-      self.painter.draw_white_stone(x, y, opacity)
-    else:
-      assert False
+        dominant_color = np.argmax(probabilities[x, y])
+        opacity = self.highest_probability_to_opacity(np.max(probabilities[x, y]))
+        foreground.add_color(dominant_color, x, y, opacity)
+    foreground.draw()
